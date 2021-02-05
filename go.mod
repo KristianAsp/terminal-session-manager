8724bd9e347e64076603faa9f0a21c8c8765eebc
Kristian Andre Aspevik <kristian.aspevik@gmail.com>
2021-02-05 23:58:20 +0100

    Load properties based on env proflies

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/stretchr/testify v1.7.0
 	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
 	github.com/urfave/cli/v2 v2.3.0
+	gopkg.in/gcfg.v1 v1.2.3 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- src/cmd/termsesh.go
+++ src/cmd/termsesh.go
@@ -4,12 +4,14 @@ import (
 	log "github.com/sirupsen/logrus"
 	easy "github.com/t-tomalak/logrus-easy-formatter"
 	"github.com/urfave/cli/v2"
+	"gopkg.in/gcfg.v1"
 	"os"
 	"terminal-session-manager/src/internal/subcommands"
 )
 
 // This needs to be set in order to be able to take a flag when building the go binary to override it
 var Version = "v1.0.0"
+var config AppConfig
 
 func main() {
 	err := runCLI(os.Args)
@@ -38,15 +40,60 @@ func runCLI(args []string) error {
 	app.Commands = append(app.Commands, subcommands.ListSubcommand())
 	app.Commands = append(app.Commands, subcommands.DescribeSubcommand())
 	app.Commands = append(app.Commands, subcommands.LoadProfileSubcommand())
-	app.Before = initLogging
+	app.Before = initialise
 	err := app.Run(args)
 	return err
 }
 
-func initLogging(c *cli.Context) error {
+func initialise(c *cli.Context) error {
+	initLogging(c.Bool("debug"))
+
+	cfg, err := loadApplicationConfiguration()
+	if err != nil { log.Fatal(err) }
+
+	config = cfg.AppConfig
+	return nil
+}
+
+func GetAppConfigValue(key string) AppConfig {
+	
+	return config.key
+}
+func loadApplicationConfiguration() (configFile, error) {
+	// Load properties file based on presence of TERMSESH_ENV environment variable
+	// If in a test environment, load the test properties.
+	var cfgFile string
+	if os.Getenv("TERMSESH_ENV") == "TEST" {
+		cfgFile = "properties/test.properties"
+	} else {
+		cfgFile = "properties/prod.properties"
+	}
+
+
+	var err error
+	var config configFile
+
+	err = gcfg.ReadFileInto(&config, cfgFile)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+type AppConfig struct {
+	ApplicationName  			string
+	DefaultConfigurationPath  	string
+	Debug						bool
+}
+
+type configFile struct {
+	AppConfig AppConfig
+}
+
+func initLogging(debug bool) error {
 	var logLevel log.Level
 	logLevel = log.InfoLevel
-	if c.Bool("debug") {
+	if debug {
 		logLevel = log.DebugLevel
 	}
 
